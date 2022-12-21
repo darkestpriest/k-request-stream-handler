a59d965b5c3a58730f2f55e4d654742aa6fe921a
Manuel Perez <mperez@yopapp.me>
2022-12-21 15:43:51 -0300

    refactor: prepares required dependencies.
    #2

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -4,13 +4,17 @@ plugins {
 
 dependencies {
 
+    //kotlin
     implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
 
     //aws
     implementation("com.amazonaws:aws-lambda-java-core:1.2.2")
 
     //tests
     testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
+    testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.1")
 
 }
 
