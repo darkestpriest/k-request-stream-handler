4e28577bf438226b4c320db87e502ad757f98a4c
Manuel Perez <mperez@yopapp.me>
2022-12-21 11:16:59 -0300

    refactor: includes basic dependencies.
    #2

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -6,6 +6,12 @@ dependencies {
 
     implementation(kotlin("stdlib-jdk8"))
 
+    //aws
+    implementation("com.amazonaws:aws-lambda-java-core:1.2.2")
+
+    //tests
+    testImplementation(kotlin("test"))
+
 }
 
 tasks.withType<Test> {
